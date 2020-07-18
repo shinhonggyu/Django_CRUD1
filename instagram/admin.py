9cacc9e568254083fbc5dbfd1852a910ee81330b
honggyu <zowoz0@naver.com>
2020-07-18 21:58:12 +0900

    ManyToManyField

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 
 # admin.site.register(Post)
 
@@ -24,4 +24,9 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
