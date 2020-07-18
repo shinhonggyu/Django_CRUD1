5265380e6715af3c55c5388f9ff72b148410335b
honggyu <zowoz0@naver.com>
2020-07-18 19:18:12 +0900

    ForeignKey

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from .models import Post
+from .models import Post, Comment
 
 # admin.site.register(Post)
 
@@ -20,3 +20,8 @@ class PostAdmin(admin.ModelAdmin):
 
     def message_length(self, post):
         return f"{len(post.message)} 글자"
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
